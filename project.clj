796d0d60397e3376eab3009e5b4858d2755120c0
Miikka Koskinen <miikka@metosin.fi>
2018-02-14 17:35:50 +0200

    Set :deploy-repositories

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/miikka/long-thread"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :deploy-repositories [["releases" :clojars]])
