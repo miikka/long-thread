9b993f1db65787fc7c58d31b997e47eef42b4e44
Miikka Koskinen <miikka@metosin.fi>
2020-01-05 19:33:48 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/miikka/long-thread"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [[lein-cloverage "1.0.10"]
-            [lein-codox "0.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-cloverage "1.1.2"]
+            [lein-codox "0.10.7"]]
   :codox {:output-path "doc"
           :source-uri "https://github.com/miikka/long-thread/blob/{version}/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}
